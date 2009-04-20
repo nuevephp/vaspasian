40c3124f89e8e4c3c14e10db83d1f5abb7ab1e90
Andrew Smith <easylancer@gmail.com>
2009-04-20 17:11:18 +0000

    added in themes module.

---
+++ modules/themes/views/expert_vision/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Expert Vision</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="mainPan">
+  <div id="topPan">
+  	<a href="index.html"><img src="images/logo.gif" alt="Expart Vision" width="230" height="44" border="0" class="logo" /></a> 
+		<h1>Lorem ipsum dolor sit amet, consectetuer adipiscingelit. Ut pretium dignissim </h1>
+		<h2>about expert vision</h2>
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li class="about"><a href="#">About Us</a></li>
+			<li><a href="#">Chat</a></li>
+			<li><a href="#">Blog</a></li>
+			<li><a href="#">Forum</a></li>
+			<li><a href="#">Clients</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+  </div>
+  <div id="toprightPan">
+  	<p>Lorem ipsum dolor sit amet,consectetuer</p>
+	<p class="more"><a href="#">view more</a></p>
+  </div>
+  
+  <div id="bodyTopPan">
+  	<p>Expert Vision is a free, tableless, W3C-compliant web design layout by <span>Template World</span>. This template has been tested and proven compatible with all major browser environments and operating systems. You are free to modify the design to suit your tastes in any way you like.</p>
+	<p>We only ask you to not remove <span>"Design by Template World"</span> and the link http://www.templateworld.com from the footer of the template.</p>
+	<p>If you are interested in seeing more of our free web template designs feel free to visit our website, Template World. We intend to add at least 25 new free templates in the coming month.</p>
+	<p class="more"><a href="#">more</a></p>
+  </div>
+  
+  <div id="leftPan">
+  	<ul>
+		<li class="positioncommunity">community</li>
+		<li class="positionsolutions"><a href="#">solutions</a></li>
+		<li class="positionmeetings"><a href="#">meetings</a></li>
+	</ul>
+	<h2>on 1st, england<br />
+	<span>M piterson</span></h2>
+	<p><span>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec Class aptent taciti lectarcu egestas luctus.</span></p>
+	<p>d nulla quis tellus ornare aliquam. Curabitur eu lectus eu massa orni mperdiet. Proin nec neque vitae lacus sollicitudin malesuada. Curab vitae magna. metus non blandit aliquet,dalacut molestie arcu ante at felis.</p>
+	<div id="bodyleftlinkboxonePan">
+			<ul>
+			<li class="comments">Comments: <span class="boldtext">23</span></li>
+			<li class="continue"><a href="#">Continue reading....</a></li>
+			</ul>
+	</div>
+		<h3>Special tips:</h3>
+		<ul class="next">
+			<li><a href="#">id nulla quis tellus ornare aliquam. Curabitur eu lectus eu</a></li>
+			<li><a href="#">massa orni</a></li>
+			<li><a href="#">mperdiet. Proin nec neque vitae lacus sollicitudin male</a></li>
+			<li><a href="#">suada. Curab</a></li>
+			<li><a href="#">vitae magna. metus non blandit aliquet,dalacut molestie</a></li>
+		</ul>
+		<h4>on 22nd, england<br />
+	<span>D johnson</span></h4>
+	<p><span>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec Class aptent taciti lectarcu egestas luctus.</span></p>
+	<p>d nulla quis tellus ornare aliquam. Curabitur eu lectus eu massa orni mperdiet. Proin nec neque vitae lacus sollicitudin malesuada. Curab vitae magna. metus non blandit aliquet,dalacut molestie arcu ante at felis.</p>
+	<div id="bodyleftlinkboxtwoPan">
+			<ul>
+			<li class="comments">Comments: <span class="boldtext">23</span></li>
+			<li class="continue"><a href="#">Continue reading....</a></li>
+			</ul>
+	</div>
+	<p class="bottompadding">&nbsp;</p>
+  </div>
+  
+  <div id="bodyrightPan">
+  	<h2>publications</h2>
+	<p>metus non blandit aliquet, massa dui sodales lacus, ut molestie arcu</p>
+	<p><span>24th may 2006</span></p>
+	<p class="toppadding-rightpantext">ante at felis.Duis vestibulum, ipsum id cursus pellent esq ue, eros leo viverra est,vel posuere felis mauris </p>
+	<p><span>26th may 2006</span></p>
+	<p>sit amet libero. Ut ipsum libero,sceler isque sed, elemen tum nec,bibend um et,orci. Quisque </p>
+	<p><span>14th june 2006</span></p>
+	<p>nec libero. Donec dui felis, laoreet pretium,congue et, ullamcorper vitae, nibh. Donec augue tortor, convallis id, aliquam eu, blandit nec,</p>
+	<p><span>20th june 2006</span></p>
+  </div>
+</div>
+
+<div id="footermainPan">
+	<div id="footerPan">
+  <ul>
+  	<li><a href="#">Home </a>| </li>
+  	<li><a href="#">About Us</a> | </li>
+  	<li><a href="#">Support </a>| </li>
+  	<li><a href="#">Clients</a> | </li>
+  	<li><a href="#">Contact </a> </li>
+  </ul>
+  
+  <ul class="info">
+  	<li class="address">23-Ambush road 0128thonhill <br />CANADA</li>
+	<li class="phone"><span>PH: </span><br />
+	  012-2568-6897 <br />
+	  012-2568-6898</li>
+	 <li class="fax"><span>FAX:</span> <br />
+	  012-2568-6897 <br />
+	  012-2568-6898</li>
+    </ul>
+   <p class="copyright">©expartvision. All right reserved.</p>
+   <ul class="templateworld">
+  	<li>Design By:</li>
+	<li><a href="http://www.templateworld.com" target="_blank">Template World</a></li>
+  </ul>
+  </div>
+</div>
+</body>
+</html>
